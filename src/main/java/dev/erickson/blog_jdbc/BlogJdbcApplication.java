c858d9a4eb04e926acd7da745f0b1dc9f17fc979
Ted Erickson <ted.erickson@comcast.net>
2024-11-13 06:18:42 -0700

    initial check in

---
+++ src/main/java/dev/erickson/blog_jdbc/BlogJdbcApplication.java
@@ -0,0 +1,13 @@
+package dev.erickson.blog_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogJdbcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/erickson/blog_jdbc/BlogJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.erickson.blog_jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
