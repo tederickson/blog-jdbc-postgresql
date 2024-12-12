25a823bd64d2cdd4dbacb368b320fa7db14cb74c
Ted Erickson <ted.erickson@comcast.net>
2024-12-12 05:59:35 -0700

    CommentService

--- src/test/java/dev/erickson/blog_jdbc/model/service/CommentServiceTest.java
+++ src/test/java/dev/erickson/blog_jdbc/model/service/CommentServiceTest.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -106,6 +107,11 @@ class CommentServiceTest {
     @Test
     void deleteById_notFound() {
         assertEquals(0, commentService.deleteById(-123L));
+    }
+
+    @Test
+    void deleteById_nullId() {
+        assertThrows(IllegalArgumentException.class, () -> commentService.deleteById(null));
     }
 
     @Test
