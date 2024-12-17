deadce40bebd77d4c6fbf9039aac53362b3136ca
Ted Erickson <ted.erickson@comcast.net>
2024-12-17 07:51:17 -0700

    PostService

--- src/main/java/dev/erickson/blog_jdbc/model/service/PostMapper.java
+++ src/main/java/dev/erickson/blog_jdbc/model/service/PostMapper.java
@@ -2,6 +2,7 @@ package dev.erickson.blog_jdbc.model.service;
 
 import dev.erickson.blog_jdbc.domain.Post;
 import dev.erickson.blog_jdbc.model.PostEntity;
+import org.springframework.util.Assert;
 
 public class PostMapper {
     public static Post toRest(final PostEntity persistPostEntity) {
@@ -12,6 +13,20 @@ public class PostMapper {
                 .content(persistPostEntity.getContent())
                 .publishedOn(persistPostEntity.getPublishedOn())
                 .updatedOn(persistPostEntity.getUpdatedOn())
+                .build();
+    }
+
+    public static PostEntity toEntity(final Post post) {
+        Assert.notNull(post.getAuthor(), "The author must not be null");
+
+        var builder = PostEntity.builder();
+        return builder
+                .id(post.getId())
+                .authorId(post.getAuthor().id())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .publishedOn(post.getPublishedOn())
+                .updatedOn(post.getUpdatedOn())
                 .build();
     }
 
